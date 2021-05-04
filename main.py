a4fc6623b2771ea96bf0b1c301b8676e815c09d7
Saurav Bhattarai <sbhattarai@himalayantechies.com>
2021-05-04 22:41:02 +0545

    Edit and update button added to edit the stored database

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import mysql.connector
 from datetime import datetime
 
 global count
+global row_id
 
 # Defining root window
 root = tkinter.Tk()
@@ -24,6 +25,8 @@ style.configure("Treeview", background="white", foreground="black", rowheight=25
 
 # Change selected color
 style.map("Treeview", background=[("selected", "blue")], foreground=[("selected", "black")])
+
+
 
 
 # Functions
@@ -53,9 +56,11 @@ def show_values():
     # Show data in table
     for row in cursor:
         if count % 2 == 0:
-            tree_view.insert(parent="", index="end", iid=count, text="", values=(row[0], row[1], row[2], row[3], row[4]), tags=("evenrow",))
+            tree_view.insert(parent="", index="end", iid=count, text="",
+                             values=(row[0], row[1], row[2], row[3], row[4]), tags=("evenrow",))
         else:
-            tree_view.insert(parent="", index="end", iid=count, text="", values=(row[0], row[1], row[2], row[3], row[4]), tags=("oddrow",))
+            tree_view.insert(parent="", index="end", iid=count, text="",
+                             values=(row[0], row[1], row[2], row[3], row[4]), tags=("oddrow",))
         count += 1
 
 
@@ -101,10 +106,92 @@ def output_number():
         # testing whether it is a integer
         float(amount_input.get())
         amount = int(amount_input.get())
-        # output_label.config(text=amount)
         submit(amount)
     except ValueError:
         output_label.config(text="Error! Please enter a number in the amount field")
+
+
+# Function to select row and have its data displayed in the entry field
+def select_row():
+
+    # Clearing all the entry fields
+    item_input.delete(0, END)
+    quantity_input.delete(0, END)
+    amount_input.delete(0, END)
+
+    # Grab record number
+    treeview_selection = tree_view.focus()
+
+    # Get values of the selected row
+    row_values = tree_view.item(treeview_selection, 'values')
+
+    # Put these values in entry box
+    item_input.insert(0, row_values[1])
+    quantity_input.insert(0, row_values[2])
+    amount_input.insert(0, row_values[3])
+
+    # Enabling update button
+    update_button.config(state="normal")
+
+
+# function to update database and treeview
+def update_record():
+    # Establishing a connection to the database
+    database = mysql.connector.connect(host="127.0.0.1", user="root", password="managedatabase",
+                                       database="restaurant_db")
+
+    # Creating a cursor instance
+    cursor = database.cursor()
+
+    # Grab record number
+    treeview_selection = tree_view.focus()
+
+    # Get values of the selected row
+    row_values = tree_view.item(treeview_selection, 'values')
+
+    # Get the id of the selected row
+    row_id = row_values[0]
+
+    try:
+        # testing whether it is a integer
+        float(amount_input.get())
+
+        # Getting the value from the entry box
+        amount = int(amount_input.get())
+        item_value = item_input.get()
+        quantity_value = quantity_input.get()
+
+        # Update in the tree view
+        tree_view.item(treeview_selection, text="", values=(row_id, item_value, quantity_value, amount, row_values[4]))
+
+        # Inserting values into table
+        sql_query = "UPDATE inventory SET item = %s, quantity = %s, amount = %s WHERE item_id = %s"
+
+        values = [item_value, quantity_value, amount, row_id]
+
+        cursor.execute(sql_query, values)
+
+        database.commit()
+
+        database.close()
+
+        # Clearing all the entry fields
+        item_input.delete(0, END)
+        quantity_input.delete(0, END)
+        amount_input.delete(0, END)
+
+        # Disabling update and select button
+        update_button.config(state="disabled")
+        select_button.config(state="disabled")
+
+    except ValueError:
+        output_label.config(text="Error! Please enter a number in the amount field")
+        database.close()
+
+
+def enable_button(event):
+    # Enable select button
+    select_button.config(state="normal")
 
 
 # GUI Layout
@@ -125,6 +212,8 @@ quantity_input = tkinter.Entry(entry_frame, borderwidth=2)
 amount_label = tkinter.Label(entry_frame, text="Amount")
 amount_input = tkinter.Entry(entry_frame, borderwidth=2)
 submit_button = tkinter.Button(entry_frame, text="Submit", command=output_number)
+select_button = tkinter.Button(entry_frame, text="Edit selected row", state="disabled", command=select_row)
+update_button = tkinter.Button(entry_frame, text="Update Record", state="disabled", command=update_record)
 output_label = tkinter.Label(entry_frame, text='')
 
 # Griding the elements onto the frame
@@ -135,8 +224,10 @@ quantity_label.grid(row=2, column=0, sticky="WE")
 quantity_input.grid(row=2, column=1, pady=12, ipady=4, padx=(8, 0), sticky="WE")
 amount_label.grid(row=3, column=0, sticky="WE")
 amount_input.grid(row=3, column=1, pady=12, ipady=4, padx=(8, 0), sticky="WE")
-submit_button.grid(row=4, column=1, pady=12, ipady=4, padx=(8, 0), sticky="WE")
-output_label.grid(row=5, column=0, columnspan=2, ipady=4, sticky="WE")
+submit_button.grid(row=4, column=1, pady=(12, 0), ipady=4, padx=(8, 0), sticky="WE")
+select_button.grid(row=5, column=0, pady=(2, 12), ipady=4, padx=(8, 0), sticky="WE")
+update_button.grid(row=5, column=1, pady=(2, 12), ipady=4, padx=(8, 0), sticky="WE")
+output_label.grid(row=6, column=0, columnspan=2, ipady=4, sticky="WE")
 
 # Results Frame layout
 
@@ -145,7 +236,7 @@ tree_scroll = tkinter.Scrollbar(results_frame)
 tree_scroll.pack(side=RIGHT, fill=Y)
 
 # Creating tree view widget
-tree_view = ttk.Treeview(results_frame, height=350, yscrollcommand=tree_scroll.set)
+tree_view = ttk.Treeview(results_frame, height=350, yscrollcommand=tree_scroll.set, selectmode="browse")
 tree_view.pack(ipady=5, expand=True, fill=BOTH)
 
 # Configuring the scroll bar
@@ -170,7 +261,17 @@ tree_view.heading("quantity", text="Quantity", anchor=W)
 tree_view.heading("amount", text="Amount", anchor=CENTER)
 tree_view.heading("date_of_purchase", text="Date of transaction", anchor=W)
 
+
+# Adding logic to the program
 show_values()
+
+# selected = tree_view.selection()
+# if len(selected) == 0:
+#     update_button.config(state="normal")
+
+
+tree_view.bind("<Button-1>", enable_button)
+
 
 # Running the main loop
 root.mainloop()
